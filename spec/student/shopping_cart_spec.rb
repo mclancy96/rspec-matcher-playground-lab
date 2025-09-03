b495adfbd5eebc830022c33c317c4f24ceefc4e8
Mike <mclancy96@gmail.com>
2025-09-03 14:16:18 -0400

    reset

--- spec/student/shopping_cart_spec.rb
+++ spec/student/shopping_cart_spec.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
 describe ShoppingCart do
-  let(:cart) {ShoppingCart.new}
-
-  it 'adds item to cart successfully' do
-    cart.add_item({name: 'Bread', price: 1.23})
-
-    expect(cart.total).to eql(1.23)
-  end
+  # ...student specs...
 end
